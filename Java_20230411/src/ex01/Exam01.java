392e4f42e71b03f4400dcc7d9d40ae28b0962cd0
wonsookyeon <129915248+wonsookyeon@users.noreply.github.com>
2023-04-12 22:59:56 +0900

    Add files via upload

--- Java_20230411/src/ex01/Exam01.java
+++ Java_20230411/src/ex01/Exam01.java
@@ -1,21 +1,37 @@
 package ex01;
 
+/* 
+ * 조원3명 자바점수를 입력받아서
+ * 자바점수 배점은 랜덤함수 이용해서 5~100값이 저장되도록 한다.
+ * 조원 총점, 평균을 출력한다.
+ */
 public class Exam01 {
 
 	public static void main(String[] args) {
+		
+		int sum=0;
+		int yj, ye, sy;
+		double average=0;
+		
+		                                    // (int)(Math.random()*50+1)+50;
+		yj  = (int)(Math.random()*51)+50;   //(int) Math.random() * (최댓값-최소값+1) + 최소값 ★★★
+		System.out.println("yj의 점수 : " + yj);
+		
+		ye  = (int)(Math.random()*51)+50;
+		System.out.println("ye의 점수 : " + ye);
+		
+		sy  = (int)(Math.random()*51)+50;
+		System.out.println("sy의 점수 : " + sy);
+		
+		
+		sum= yj+ye+sy;
+		average = sum/3.0;
+		
+	
+		System.out.println("총점 : " + sum);
+		System.out.printf("평균 : %.2f" , average );  // ★기억하자 평균은 %f .2를 해주면 소수점 2번째까지 출력
+		
 
-		for (int i=0; i<3; i++)  // i는 (0,1,2) 3번 실행  
-			for (int j=0; j<5; j++) // j 는 (0,1,2,3,4) 5번 실행
-				System.out.println(i + "+" + j);
-		
-		
-		/* i가 0 일때 j는 0,1,2,3,4
-		 * i가 1 일때 j는 0,1,2,3,4 실행된다.
-		 */
-		
-		
-		
-		
 	}
 
 }
