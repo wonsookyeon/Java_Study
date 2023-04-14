de0c932745df22006161668a84131110190a065b
wonsookyeon <129915248+wonsookyeon@users.noreply.github.com>
2023-04-14 15:30:34 +0900

    Add files via upload

---
+++ java_20230414/src/ex01/FunctionExam06.java
@@ -0,0 +1,40 @@
+package ex01;
+
+//합수 
+/*
+ *  함수를 호출하면서 인자값 하나 전달(정수)하면 함수는 1부터 전달한 인자값까지 더해서
+ *  더한 결과값을 호출함수에게 반환해서 출력
+ */
+public class FunctionExam06 {
+
+	public static void main(String[] args) {
+		System.out.println("프로그램 시작");
+		
+		
+		int total = sumFunction(10);     //  total이라는 변수이름으로 return한 값을 받아줌.
+										//(int num) 으로 올라오기때문에 ()에 정수지정.
+		
+		if(total%2 == 0)
+			System.out.println("짝수 " + total);
+		else
+			System.out.println("홀수 " + total);
+				
+		
+		System.out.println("Total : " + total);
+		
+		
+		System.out.println("프로그램 끝" + ", ");
+	}
+
+		static int sumFunction(int num) {  // 1. 숫자하나를 받아서
+			int sum = 0; 
+			
+			for(int i=1; i<=num; i++) // 2. 1부터 숫자까지
+				sum +=i;   // 3. 더해간다.
+			
+			return sum;  // 4. 더한 값을 return해서 위(14줄)로 올려줌↑
+			
+	}
+	
+		
+}
