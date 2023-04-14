7b74137e407e6034d6bab85c4b242981a0ce679e
wonsookyeon <129915248+wonsookyeon@users.noreply.github.com>
2023-04-14 15:29:35 +0900

    Add files via upload

--- Java_20230413/src/ex01/Exam04.java
+++ Java_20230413/src/ex01/Exam04.java
@@ -6,7 +6,7 @@ public class Exam04 {
 		
 //		int[] num = new int[5];
 		
-		int[][] num = new int[4][3];  //[행][열]
+		int[][] num = new int[4][3];  //[행][열]		
 
 		num[2][1] = 100;  // 2행 1열에 100 입력
 
@@ -22,6 +22,18 @@ public class Exam04 {
 			
 			System.out.println();
 		}
+		
+/*		for(int i=0; i<num.length; i++)
+			for(int j=0; j=num[i].length; j++)
+				num[i][j] = (int)(Math.random()*100)+1;
+		
+		for(int i=0; i<num.length; i++)
+			for(int j=0; j=num[i].length; j++) // j 는 열 //행의 길이가 다를때는 i의 길이로..
+				System.out.print(num[i][j] + ",");
+							
+		System.out.println();
+						}
+*/		
 	}
 
 }
